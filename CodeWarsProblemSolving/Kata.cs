8fbed41750109341b2a4a73ede8857431f40292b
salemovic <jakihop889@gamil.com>
2025-05-22 13:15:53 +0300

    add new solution

--- CodeWarsProblemSolving/Kata.cs
+++ CodeWarsProblemSolving/Kata.cs
@@ -3,6 +3,526 @@
     public static class Kata
     {
 
+        public static int DescendingOrderTest(int num)
+        {
+
+            return Convert.ToInt32(string.Join("", num.ToString().ToCharArray().OrderByDescending(x => x).ToArray()));
+        }
+
+        public static string Longest(string s1, string s2)
+        {
+            var newword = s1 + s2;
+            var letters = newword.ToCharArray().Distinct().ToArray();
+            Array.Sort(letters);
+            return string.Join("", letters);
+        }
+
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            return listOfItems.Where(x => x is int).Cast<int>().ToList();
+        }
+
+        public static int FindShort(string s)
+        {
+            var words = s.Split(' ');
+            int minLengthWord = words[0].Length;
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (words[i].Length < minLengthWord)
+                {
+                    minLengthWord = words[i].Length;
+                }
+            }
+
+            return minLengthWord;
+        }
+        public static int CockroachSpeed(double x)
+        {
+            //#Happy Coding! ^_^
+            return Convert.ToInt32(Math.Floor((x * 1000) * 100 / 3600));
+        }
+
+        public static int HexToDec(string hexString)
+        {
+            if (hexString.StartsWith('-'))
+            {
+                hexString = hexString.Substring(1);
+                return -Convert.ToInt32(hexString, 16);
+            }
+
+            return Convert.ToInt32(hexString, 16);
+        }
+        public static int RentalCarCost(int d)
+        {
+            // your code
+            return d >= 7 ? d * 40 - 50 : d >= 3 ? d * 40 - 20 : d * 40;
+        }
+
+        public static int Gimme(double[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] < inputArray.Max() && inputArray[i] > inputArray.Min())
+                    return i;
+            }
+            return 0;
+        }
+
+        public static int NoBoringZeros(int n)
+        {
+            return int.Parse(n.ToString().TrimEnd('0'));
+        }
+
+        public static int DescendingOrder(int num)
+        {
+            // Bust a move right here
+
+            var numbers = num.ToString().Split("");
+
+            return 0;
+        }
+
+        public static string HighAndLow(string numbers)
+        {
+            // Code here or
+            var numbersList = numbers.Split(' ').Select(int.Parse).ToList();
+
+            numbersList.Sort();
+
+            return string.Join(' ', numbersList.Last(), numbersList.First());
+
+        }
+
+        public static string ReverseWords(string str)
+        {
+            // Split the string into words
+            string[] words = str.Split(' ');
+
+            var reversedWord = new List<string>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                reversedWord.Add(revers(words[i]));
+            }
+
+            return string.Join(" ", reversedWord);
+
+        }
+
+        public static string revers(string word)
+        {
+            var reversedWord = string.Empty;
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversedWord += word[i];
+            }
+            return reversedWord;
+        }
+
+
+        public static double Arithmetic(double a, double b, string op)
+        {
+            switch (op)
+            {
+                case "add":
+                    return a + b;
+                case "subtract":
+                    return a - b;
+                case "multiply":
+                    return a * b;
+                case "divide":
+                    return a / b;
+                default:
+                    return 0;
+            }
+        }
+        public static List<int> ReverseList(List<int> list)
+        {
+
+
+            return list.AsEnumerable().Reverse().ToList();
+        }
+        public static int TwiceAsOld(int dadYears, int sonYears)
+        {
+            // Add you code here.
+            return Math.Abs((sonYears * 2) - dadYears);
+        }
+        public static List<int>[] PowersOfTwo(int n)
+        {
+            var list = new List<int>();
+            for (int i = 0; i <= n; i++)
+            {
+                list.Add(2 << i);
+            }
+            return new List<int>[] { list };
+
+        }
+        public static bool CheckForFactor(int num, int factor)
+        {
+            // code here
+
+            return num % factor == 0 ? true : false;
+        }
+        public static string StringsSum(string s1, string s2)
+        {
+            // Write your solution here.
+            int sum = 0;
+            if (int.TryParse(s1, out int reslut))
+            {
+                sum += reslut;
+            }
+
+            if (int.TryParse(s2, out int result))
+            {
+                sum += result;
+            }
+
+
+
+            return sum.ToString();
+
+        }
+
+        public static string TwoSort(string[] s)
+        {
+            Array.Sort(s, StringComparer.Ordinal);
+
+
+            string text = string.Empty;
+
+            for (int i = 0; i < s[0].Length; i++)
+            {
+                text += s[0][i];
+
+                if (i != s[0].Length - 1)
+                {
+                    text += "***";
+                }
+            }
+            return text.Trim();
+
+        }
+
+        public static int Litres(double time)
+        {
+
+            return Convert.ToInt32(Math.Floor(time * 0.5));
+        }
+        public static int CountSheeps(bool[] sheeps)
+        {
+            //TODO
+
+            if (sheeps == null || sheeps.Length == 0)
+                return 0;
+
+
+            int count = 0;
+            for (int i = 0; i < sheeps.Length; i++)
+            {
+                if (sheeps[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static bool Check(object[] a, object x)
+        {
+            return a.Contains(x);
+
+        }
+        public static bool Hero(int bullets, int dragons)
+        {
+            //Do Some Magic...
+            return dragons * 2 >= bullets ? true : false;
+        }
+
+        public static int SumMix(object[] x)
+        {
+            int sum = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+
+                sum += Convert.ToInt32(x[i]);
+            }
+            return sum;
+        }
+
+        public static int SquareSum(int[] numbers)
+        {
+            return Convert.ToInt32(numbers.Select(x => Math.Pow(x, 2)).Sum());
+        }
+
+        public static string Position(char alphabet)
+        {
+            if (!char.IsLower(alphabet))
+            {
+                return "";
+            }
+            Dictionary<char, int> alphabetDict = new Dictionary<char, int>();
+            int i = 1;
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+
+                alphabetDict[c] = i;
+                i++;
+            }
+
+            return $"Position of alphabet: {alphabetDict[alphabet]}";
+        }
+
+        public static int[] CountPositivesSumNegatives(int[] input)
+        {
+
+            if (input == null || input.Length == 0)
+                return new int[] { };
+
+            int countPostiveNumbers = 0;
+
+            int sumNegativeNumbers = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] > 0)
+                {
+                    countPostiveNumbers++;
+                }
+                else
+                {
+                    sumNegativeNumbers += input[i];
+                }
+
+            }
+
+            return new int[] { countPostiveNumbers, sumNegativeNumbers };
+        }
+        public static float Combat(float health, float damage)
+        {
+            return health < damage ? 0 : health - damage;
+        }
+
+        public static int SaleHotDogs(int n)
+        {
+            return n >= 10 ? n * 90 : n >= 5 && n < 10 ? n * 95 : n * 100;
+        }
+
+        public static string Likes(string[] name)
+        {
+            if (name.Length == 0)
+                return "no one likes this";
+
+            if (name.Length == 1)
+                return $"{name[0]} likes this";
+
+            if (name.Length == 2)
+                return $"{name[0]} and {name[1]} like this";
+
+            if (name.Length == 3)
+                return $"{name[0]}, {name[1]} and {name[2]} like this";
+
+            return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
+
+        }
+        public static string Reverse(string text)
+        {
+            var words = text.Trim().Split(' ');
+            var result = string.Empty;
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                result += words[i] + " ";
+            }
+
+
+            return result.Trim();
+        }
+        public static string Derive(double coefficient, double exponent)
+        {
+            return $"{coefficient * exponent}x^{exponent - 1}";
+        }
+
+        public static int SumDigits(int number)
+        {
+
+            var numberString = number.ToString().Where(char.IsDigit).Select(c => int.Parse(c.ToString())).ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                sum += Math.Abs(numberString[i]);
+            }
+
+            return sum;
+        }
+
+        public static int[] DivisibleBy(int[] numbers, int divisor)
+        {
+
+            var list = new List<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % divisor == 0)
+                {
+                    list.Add(numbers[i]);
+                }
+            }
+            return list.ToArray();
+        }
+
+        public static int[] distinct(int[] a)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (!list.Contains(a[i]))
+                {
+                    list.Add(a[i]);
+                }
+            }
+            return list.ToArray();
+        }
+
+        public static int Move(int position, int roll)
+        {
+            int move = roll * 2;
+            int newPosition = position + move;
+            return newPosition;
+        }
+
+        public static string Solve(string s)
+        {
+            //Do Some Magic
+            int lowercaseCount = 0;
+            int uppercaseCount = 0;
+
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLower(s[i]))
+                {
+                    lowercaseCount++;
+                }
+                else if (char.IsUpper(s[i]))
+                {
+                    uppercaseCount++;
+                }
+            }
+
+            if (lowercaseCount > uppercaseCount)
+            {
+                return s.ToLower();
+            }
+
+            return uppercaseCount > lowercaseCount ? s.ToUpper() : s.ToLower();
+
+
+        }
+        public static string ToJadenCase(this string phrase)
+        {
+            string[] words = phrase.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+
+                words[i] = words[i].Substring(0, 1).ToUpper() + words[i].Substring(1);
+
+            }
+            return String.Join(' ', words);
+        }
+
+        public static int OtherAngle(int a, int b)
+        {
+            return 180 - (a + b);
+        }
+
+        public static int[] MergeArrays(int[] arr1, int[] arr2)
+        {
+            var leftArrayPTR = 0;
+            var rightArrayPTR = 0;
+            var newList = new List<int>();
+
+            //sort array before merging
+            Array.Sort(arr1);
+            Array.Sort(arr2);
+
+
+            while (leftArrayPTR < arr1.Length && rightArrayPTR < arr2.Length)
+            {
+                if (arr1[leftArrayPTR] < arr2[rightArrayPTR])
+                {
+                    if (!newList.Contains(arr1[leftArrayPTR]))
+                    {
+                        newList.Add(arr1[leftArrayPTR]);
+                    }
+
+                    leftArrayPTR++;
+                }
+                else if (arr2[rightArrayPTR] < arr1[leftArrayPTR])
+                {
+                    if (!newList.Contains(arr2[rightArrayPTR]))
+                    {
+                        newList.Add(arr2[rightArrayPTR]);
+                    }
+                    rightArrayPTR++;
+                }
+                else // if the values are equal
+
+                {
+                    if (!newList.Contains(arr1[leftArrayPTR]))
+                    {
+                        newList.Add(arr1[leftArrayPTR]);
+                    }
+                    leftArrayPTR++;
+                    rightArrayPTR++;
+                }
+
+            }
+
+            while (leftArrayPTR < arr1.Length)
+            {
+                if (!newList.Contains(arr1[leftArrayPTR]))
+                {
+                    newList.Add(arr1[leftArrayPTR]);
+                }
+                leftArrayPTR++;
+            }
+
+            while (rightArrayPTR < arr2.Length)
+            {
+                if (!newList.Contains(arr2[rightArrayPTR]))
+                {
+                    newList.Add(arr2[rightArrayPTR]);
+                }
+                rightArrayPTR++;
+            }
+
+
+            Array.Sort(newList.ToArray());
+            return newList.ToArray();
+        }
+        public static long[] Digitize(long n)
+        {
+            if (n <= 0)
+            {
+                return new long[] { 0 };
+            }
+            var list = n.ToString().ToCharArray();
+            var result = list.Select(x => Convert.ToInt64(x.ToString())).ToArray();
+            Array.Reverse(result);
+            return result;
+        }
+        public static string Problem(String a)
+        {
+            //magic code 
+            if (double.TryParse(a, out double result))
+            {
+                return (result * 50 + 6).ToString();
+            }
+            return "Error";
+        }
         public static bool SpeakEnglish(string sentence)
         {
 
--- CodeWarsProblemSolving/Program.cs
+++ CodeWarsProblemSolving/Program.cs
@@ -1,5 +1,155 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+
+
+using CodeWarsProblemSolving;
+Console.WriteLine(Kata.DescendingOrderTest(42145));
+
+//Console.WriteLine(Kata.Longest("xyaabbbccccdefww", "xxxxyyyyabklmopq"));
+
+//foreach (var item in Kata.GetIntegersFromList(new List<object>() { 1, "a", "b", 0, 15 }))
+//{
+//    Console.WriteLine(item);
+//}
+
+//Console.WriteLine(Kata.FindShort("bitcoin take over the world maybe who knows perhaps"));
+//Console.WriteLine(Kata.FindShort("Let's travel abroad shall we"));
+
+
+//Console.WriteLine(Kata.CockroachSpeed(1.08));
+//Console.WriteLine(Kata.CockroachSpeed(1.09));
+//Console.WriteLine(Kata.CockroachSpeed(0));
+
+//Console.WriteLine(Kata.HexToDec("a"));
+//Console.WriteLine(Kata.HexToDec("-a"));
+
+//Console.WriteLine(Kata.RentalCarCost(1));
+//Console.WriteLine(Kata.RentalCarCost(3));
+//Console.WriteLine(Kata.RentalCarCost(7));
+//Console.WriteLine(Kata.Gimme(new double[] { 2, 3, 1 }));
+//Console.WriteLine(Kata.Gimme(new double[] { 5, 10, 14 }));
+
+//Console.WriteLine(Kata.NoBoringZeros(12450));
+//Console.WriteLine(Kata.NoBoringZeros(9600000));
+
+
+//Console.WriteLine(Kata.HighAndLow("1 2 3 4 5"));
+//Console.WriteLine(Kata.HighAndLow("1 2 -3 4 5"));
+//Console.WriteLine(Kata.HighAndLow("1 9 3 4 -5"));
+
+//Console.WriteLine(Kata.ReverseWords("This is an example!"));
+
+//Console.WriteLine(Kata.Arithmetic(5, 2, "add"));
+//Console.WriteLine(Kata.Arithmetic(8, 1, "subtract"));
+
+//foreach (var item in Kata.ReverseList(new List<int> { 1, 2, 3, 4 }))
+//{
+//    Console.WriteLine(item);
+//}
+
+//Console.WriteLine(Kata.TwiceAsOld(30, 0));
+//Console.WriteLine(Kata.TwiceAsOld(30, 7));
+//Console.WriteLine(Kata.TwiceAsOld(45, 30));
+
+//Console.WriteLine(Kata.CheckForFactor(10, 2));
+//Console.WriteLine(Kata.CheckForFactor(9, 2));
+
+//Console.WriteLine(Kata.StringsSum("4", "5"));
+
+//Console.WriteLine(Kata.TwoSort(new[] { "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps" }));
+//Console.WriteLine(Kata.TwoSort(new[] { "turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones" }));
+
+//Console.WriteLine(Kata.Litres(3));
+//Console.WriteLine(Kata.Litres(6.7));
+//Console.WriteLine(Kata.Litres(11.8));
+
+//Console.WriteLine(Kata.CountSheeps(new bool[] { true, false, true }));
+
+//Console.WriteLine(Kata.Check(new object[] { 66, 101 }, 66));
+
+//Console.WriteLine(Kata.Hero(1500, 751));
+
+
+
+//Console.WriteLine(Kata.SumMix(new object[] { 9, 3, "7", "3" }));
+//Console.WriteLine(Kata.SumMix(new object[] { "5", "0", 9, 3, 2, 1, "9", 6, 7 }));
+//Console.WriteLine(Kata.SumMix(new object[] { "3", 6, 6, 0, "5", 8, 5, "6", 2, "0" }));
+
+//Console.WriteLine(Kata.SquareSum(new int[] { 1, 2, 2 }));
+//Console.WriteLine(Kata.SquareSum(new int[] { 1, 2 }));
+//Console.WriteLine(Kata.SquareSum(new int[] { 5, 3, 4 }));
+//Console.WriteLine(Kata.Position('a'));
+//Console.WriteLine(Kata.Position('z'));
+//Console.WriteLine(Kata.Position('e'));
+
+//foreach (var item in Kata.CountPositivesSumNegatives(new int[] { }))
+//{
+//    Console.WriteLine(item);
+//}
+
+//Console.WriteLine(Kata.Combat(100, 5));
+//Console.WriteLine(Kata.Combat(20, 30));
+
+//Console.WriteLine(Kata.SaleHotDogs(1));
+//Console.WriteLine(Kata.SaleHotDogs(4));
+//Console.WriteLine(Kata.SaleHotDogs(5));
+//Console.WriteLine(Kata.SaleHotDogs(10));
+
+//Console.WriteLine(Kata.Likes(new string[] { "Jacob", "Alex" }));
+//Console.WriteLine(Kata.Likes(new string[] { "Max", "John", "Mark" }));
+//Console.WriteLine(Kata.Likes(new string[] { "Alex", "Jacob", "Mark", "Max" }));
+//Console.WriteLine(Kata.Likes(new string[] { "Alex", "Jacob", "Mark", "Max", "test" }));
+
+
+//Console.WriteLine(Kata.Reverse("Hello World"));
+//Console.WriteLine(Kata.Reverse("Hi There."));
+//Console.WriteLine(Kata.Reverse("I am an expert at this"));
+
+//Console.WriteLine(Kata.Derive(7, 8));
+//Console.WriteLine(Kata.Derive(5, 9));
+
+//Console.WriteLine(Kata.SumDigits(10));
+//Console.WriteLine(Kata.SumDigits(99));
+//Console.WriteLine(Kata.SumDigits(-32));
+
+//foreach (var item in Kata.DivisibleBy(new int[] { 1, 2, 3, 4, 5, 6 }, 2))
+//{
+//    Console.WriteLine(item);
+//}
+
+//foreach (var item in Kata.distinct(new int[] { 1, 1, 2 }))
+//{
+//    Console.Write(item + " ");
+//}
+
+//Console.WriteLine(Kata.Move(3, 6));
+//Console.WriteLine(Kata.Move(0, 4));
+
+//Console.WriteLine(Kata.Solve("code"));
+//Console.WriteLine(Kata.Solve("CODe"));
+//Console.WriteLine(Kata.Solve("COde"));
+//Console.WriteLine(Kata.Solve("Code"));
+
+//Console.WriteLine(Kata.ToJadenCase("How can mirrors be real if our eyes aren't real"));
+
+//Console.WriteLine(Kata.OtherAngle(30, 60));
+
+//foreach (var item in Kata.MergeArrays(new[] { 1, 3, 5, 7, 9 }, new[] { 10, 8, 6, 4, 2 }))
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//foreach (var item in Kata.Digitize(35231))
+//{
+//    Console.WriteLine(item);
+//}
+
+//Console.WriteLine(Kata.Problem("1"));
+//Console.WriteLine(Kata.Problem("5"));
+//Console.WriteLine(Kata.Problem("hello"));
+//Console.WriteLine(Kata.Problem("1.2"));
 //Console.WriteLine(Kata.Order("is2 Thi1s T4est 3a"));
 //Console.WriteLine(Kata.Order("4of Fo1r pe6ople g3ood th5e the2"));
 
